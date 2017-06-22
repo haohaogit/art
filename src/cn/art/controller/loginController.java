5dd9a2808a87efd5c04ba8ac215f443d39f1d4e6
Administrator <Administrator@X2IURHU9LL6PSFZ>
2017-06-22 19:23:19 +0800

    简单的界面演示

---
+++ src/cn/art/controller/loginController.java
@@ -0,0 +1,33 @@
+package cn.art.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/html")
+public class loginController {
+	
+	
+	@RequestMapping("customerLogin")
+	public String comtomlogin(HttpServletRequest request){
+		String account = (String) request.getAttribute("account");
+		String password = (String) request.getAttribute("password");
+		System.out.println(account+"  "+password);
+		if("111".equals(account)&&"222".equals(password)){
+			return "redirect:/html/corpus.do";
+		}else{
+			return "customerLogin";
+		}
+	}
+	@RequestMapping("corpus")
+	public String comtomcorpus(HttpServletRequest request){
+		String name = "wangzhe";
+    	request.setAttribute("name",name);
+    	System.out.println("1111111111");
+		return "corpus";
+	}
+
+}
